f36863bbca244081fb994f11cd9231d445081d97
John Barbone <jbarbone@johns-macbook-pro.local>
2016-11-09 13:11:53 -0500

    HELLO

--- GameOfLife/GameOfLife/ViewController.swift
+++ GameOfLife/GameOfLife/ViewController.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 //hello
+//its me
+//i was wondering
+//if after all these years
+//you'd finally like to meet
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
